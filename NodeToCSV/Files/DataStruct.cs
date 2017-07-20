2602892f53ffa93b9f0f2647d47652fcd0fbfc6e
Daniel Janowski <daniel.janowski@666510.com>
2017-07-20 23:06:54 +0200

    * Added datastruct and abstract methods for serialization in NodeBase

---
+++ NodeToCSV/Files/DataStruct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeToCSV.Files
+{
+	public class DataStruct
+	{
+		public string uniqueID;
+
+
+		override public string ToString()
+		{
+			return "DataStruct: [" + uniqueID + "]";
+		}
+
+		public string Save()
+		{
+			return uniqueID;
+		}
+
+		public string Export()
+		{
+			return uniqueID;
+		}
+	}
+}
--- NodeToCSV/GraphElements/NodeBase.cs
+++ NodeToCSV/GraphElements/NodeBase.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace NodeToCSV.GraphElements
 {
-	public class NodeBase : GraphElementBase
+	public abstract class NodeBase : GraphElementBase
 	{
+		public abstract void LoadFromDataRow(Files.DataStruct dataRow);
+		public abstract Files.DataStruct ToSaveFileDataRow();
+		public abstract Files.DataStruct ToExportFileDataRow();
+
+		public void BeginDragnDrop(MouseButtonEventArgs e)
+		{
+
+		}
+		public void UpdateDragnDrop(MouseEventArgs e)
+		{
+
+		}
+		public void EndDragnDrop(MouseButtonEventArgs e)
+		{
+
+		}
 	}
 }
