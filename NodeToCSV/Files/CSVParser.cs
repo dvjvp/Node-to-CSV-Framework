8bf919f3ac63ea460f09dcedcf378ef12ed8d11a
Daniel Janowski <daniel.janowski@666510.com>
2017-07-22 18:50:17 +0200

    * Grid tile size is now modifiable in ResourceDictionary "ColoursLibrary.xaml" * Started working on CSVParser.cs * Setting app icon

---
+++ NodeToCSV/Files/CSVParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeToCSV.Files
+{
+	public static class CSVParser
+	{
+		public const string SAVE_FILE_HEADER = "Node editor sample";
+		public const string VERSION_ID = "V. 0.1";
+
+		public const string EXPORT_FILE_HEADER = "---,Column0,Column1";
+	}
+}
