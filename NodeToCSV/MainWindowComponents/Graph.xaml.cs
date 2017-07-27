a9a5d0d3409602b3554de6b13d262dfecb861396
Daniel Janowski <daniel.janowski@666510.com>
2017-07-27 12:09:20 +0200

    * Basic graph zooming and panning

--- NodeToCSV/MainWindowComponents/Graph.xaml.cs
+++ NodeToCSV/MainWindowComponents/Graph.xaml.cs
@@ -21,10 +21,145 @@ namespace NodeToCSV.MainWindowComponents
 	public partial class Graph : UserControl
 	{
 
+
 		public Graph()
 		{
 			InitializeComponent();
+
+			//SetCanvasSizeToInfinity();
 		}
 
+		private void SetCanvasSizeToInfinity()
+		{
+			double infinity = 99999999999;
+
+
+			CanvasContainer.Width = infinity;
+			CanvasContainer.Height = infinity;
+			BackgroundCanvas.Width = infinity;
+			BackgroundCanvas.Height = infinity;
+		}
+
+		#region Transform utility
+
+		public Point ViewCenter
+		{
+			get
+			{
+				return TotalTransform.Inverse.Transform(
+					new Point(ViewportBorders.ActualWidth, ViewportBorders.ActualHeight));
+			}
+		}
+
+		#endregion
+
+		#region Panning graph
+
+		private bool panCanvasInProgress = false;
+		private Point panCanvasStartingMousePosition;
+		private Point panCavasStartingTranslation;
+
+		public void PanCanvas(Vector panOffset)
+		{
+			double X = Math.Min(GraphTranslation.X + panOffset.X, 0);
+			double Y = Math.Min(GraphTranslation.Y + panOffset.Y, 0);
+
+			//for math and utility functions to work properly
+			GraphTranslation.X = X;
+			GraphTranslation.Y = Y;
+
+			//actual view offset
+			var margin = CanvasContainer.Margin;
+			margin.Left = X;
+			margin.Top = Y;
+			CanvasContainer.Margin = margin;
+		}
+		public void SetCanvasPosition(Point leftUpCorner)
+		{
+			double X = Math.Min(leftUpCorner.X, 0);
+			double Y = Math.Min(leftUpCorner.Y, 0);
+
+			//for math and utility functions to work properly
+			GraphTranslation.X = X;
+			GraphTranslation.Y = Y;
+
+			//actual view offset
+			var margin = CanvasContainer.Margin;
+			margin.Left = X;
+			margin.Top = Y;
+			CanvasContainer.Margin = margin;
+		}
+
+		private void StartPanCanvas(object sender, MouseButtonEventArgs e)
+		{
+			//pan only with right mouse button
+			if (e.RightButton != MouseButtonState.Pressed)
+			{
+				return;
+			}
+
+			panCanvasInProgress = true;
+			panCanvasStartingMousePosition = e.GetPosition(CanvasContainer);
+			panCavasStartingTranslation = new Point(GraphTranslation.X, GraphTranslation.Y);
+
+			CanvasContainer.MouseMove += PanCanvas_MouseMoved;
+			CanvasContainer.Cursor = Cursors.SizeAll;
+		}
+
+		private void PanCanvas_MouseMoved(object sender, MouseEventArgs e)
+		{
+			Vector totalCursorMovement = e.GetPosition(CanvasContainer) - panCanvasStartingMousePosition;
+			SetCanvasPosition(panCavasStartingTranslation + totalCursorMovement);
+		}
+
+		private void EndPanCanvas(object sender, MouseButtonEventArgs e)
+		{
+			panCanvasInProgress = false;
+
+			CanvasContainer.MouseMove -= PanCanvas_MouseMoved;
+			CanvasContainer.Cursor = Cursors.Arrow;
+		}
+
+		#endregion
+
+		#region Zooming graph
+
+		const double ZOOM_SPEED = 0.05;
+		const double ZOOM_MIN = 0.15;
+		const double ZOOM_MAX = 3.0;
+
+		private void ZoomCanvasToMouseOrFromCenter(object sender, MouseWheelEventArgs e)
+		{
+			bool zoomingIn = e.Delta > 0;
+			Point oldCenter = zoomingIn ? e.GetPosition(CanvasContainer) : ViewCenter;
+			double newZoom = GraphScale.ScaleX;
+
+			if (zoomingIn)
+			{
+				newZoom += ZOOM_SPEED;
+			}
+			else
+			{
+				newZoom -= ZOOM_SPEED;
+			}
+
+			SetZoom(newZoom);
+
+			Vector offset = oldCenter - (zoomingIn ? e.GetPosition(CanvasContainer) : ViewCenter);
+			PanCanvas(offset);
+		}
+
+		public void SetZoom(double newValue)
+		{
+			newValue = Math.Max(newValue, ZOOM_MIN);
+			newValue = Math.Min(newValue, ZOOM_MAX);
+
+			GraphScale.ScaleX = newValue;
+			GraphScale.ScaleY = newValue;
+
+			Console.WriteLine("Zoom: " + newValue);
+		}
+
+		#endregion
 	}
 }
